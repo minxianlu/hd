311aa95073c385a409f0ab078d44b1cebcd77655
minxianlu <852214033@qq.com>
2019-04-12 15:38:31 +0800

    新增了常量基类，未写入内容

---
+++ src/main/java/com/mxl/framework/web/base/baseConstant.java
@@ -0,0 +1,14 @@
+package com.mxl.framework.web.base;
+
+/**
+ * @author mxl
+ * @title: baseConstant
+ * @projectName hd
+ * @description: TODO
+ * @date 2019/4/1215:35
+ */
+public class baseConstant {
+
+
+
+}
